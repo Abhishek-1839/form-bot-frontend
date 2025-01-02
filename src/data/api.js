ad11cb5f85ca0d12ef0febaffd066aebef2a1205
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2025-01-02 17:34:57 +0530

    api file

--- src/data/api.js
+++ src/data/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://form-bot-backend-i6mfkd1sn-abhishek-1839s-projects.vercel.app/",
     headers: {
         "Content-Type": "application/json",
     },
